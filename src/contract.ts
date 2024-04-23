696c07fc7d42d706bd3fdf9f680256fc136955b8
Gert van den Buijs <gertvandenbuijs@hotmail.com>
2024-04-23 14:52:36 +0200

    export contracts

---
+++ src/contract.ts
@@ -0,0 +1,37 @@
+export {
+  AlternateItem,
+  AlternateI,
+  AlternateConfigI,
+} from "./alternate/contracts/alternate";
+export { ColorsI, ColorsConfigI } from "./colors/contracts/colors";
+export { HexColor } from "./colors/contracts/hex";
+export {
+  AdditionalColorItem,
+  AdditionalColorI,
+  AdditionalColorIConfigI,
+} from "./colors/contracts/color-additional";
+export {
+  ColorVariantI,
+  ColorVariantConfigI,
+} from "./colors/contracts/color-variant";
+export { RadiusI, RadiusConfigI } from "./radius/contracts/radius";
+export { ThemeI, ThemeConfigI } from "./theme/contracts/theme";
+export {
+  ThemeVariantsI,
+  ThemeVariantsConfigI,
+} from "./theme/contracts/theme-variant";
+export { TypographyFontsFamilyConfigI } from "./typography/contracts/family";
+export { TypographyFontI } from "./typography/contracts/font";
+export {
+  TypographyFontsI,
+  TypographyFontsConfigI,
+} from "./typography/contracts/fonts";
+export { TypographyFontsScaleLineHeightConfigI } from "./typography/contracts/line-height";
+export { TypographyFontsScaleConfigI } from "./typography/contracts/scale";
+export { TypographyFontsScaleSizeConfigI } from "./typography/contracts/size";
+export {
+  TypographyI,
+  TypographyConfigI,
+} from "./typography/contracts/typography";
+export { SpacingI } from "./utility/contracts/spacing";
+export { UtilityI } from "./utility/contracts/utility";
--- src/contracts.ts
+++
@@ -1,4 +0,0 @@
-export { ThemeI } from "./theme/contracts/theme";
-export { ThemeConfigI } from "./theme/contracts/theme";
-export { ThemeVariantsI } from "./theme/contracts/theme-variant";
-export { ThemeVariantsConfigI } from "./theme/contracts/theme-variant";
