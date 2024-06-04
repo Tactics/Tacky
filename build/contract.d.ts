752e69e074db60babdb5b9d65466779302da3d13
Gert van den Buijs <gertvandenbuijs@hotmail.com>
2024-06-04 09:32:14 +0200

    build

---
+++ build/contract.d.ts
@@ -0,0 +1,34 @@
+export { b as AlternateConfigI, a as AlternateI, A as AlternateItem } from './alternate-j1k3Yp03.js';
+import { a as ThemeI, T as ThemeConfigI } from './theme-BMOx9_37.js';
+export { c as ColorsConfigI, C as ColorsI, g as Radius, d as RadiusConfigI, R as RadiusI, e as RadiusKey, f as RadiusKeys, y as Spacing, S as SpacingI, w as SpacingKey, x as SpacingKeys, v as TypographyConfigI, m as TypographyFont, i as TypographyFontI, j as TypographyFontKey, l as TypographyFontKeys, o as TypographyFontWeight, k as TypographyFontWeightKey, n as TypographyFontWeightKeys, p as TypographyFontsConfigI, h as TypographyFontsFamilyConfigI, b as TypographyFontsI, q as TypographyFontsKey, r as TypographyFontsKeys, s as TypographyFontsScaleConfigI, t as TypographyFontsScaleSizeConfigI, u as TypographyI, U as UtilityI } from './theme-BMOx9_37.js';
+export { a as AdditionalColorI, b as AdditionalColorIConfigI, A as AdditionalColorItem, H as HexColor } from './color-additional-_M7QfRi_.js';
+export { d as ColorVariant, a as ColorVariantConfigI, C as ColorVariantI, b as ColorVariantKey, c as ColorVariantKeys } from './color-variant-D9WhE53m.js';
+
+interface ThemeVariantsI {
+    standard: ThemeI;
+    inverted: ThemeI;
+    contrast: ThemeI;
+}
+interface ThemeVariantsConfigI {
+    standard?: ThemeConfigI;
+    inverted?: ThemeConfigI;
+    contrast?: ThemeConfigI;
+}
+
+interface TypographyFontsScaleLineHeightConfigI {
+    xs?: string | number;
+    sm?: string | number;
+    md?: string | number;
+    lg?: string | number;
+    xl?: string | number;
+    "2xl"?: string | number;
+    "3xl"?: string | number;
+    "4xl"?: string | number;
+    "5xl"?: string | number;
+    "6xl"?: string | number;
+    "7xl"?: string | number;
+    "8xl"?: string | number;
+    "9xl"?: string | number;
+}
+
+export { ThemeConfigI, ThemeI, type ThemeVariantsConfigI, type ThemeVariantsI, type TypographyFontsScaleLineHeightConfigI };
